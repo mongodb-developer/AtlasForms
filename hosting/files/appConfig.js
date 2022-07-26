cca3f0135f3dffa33ed4a21474d38608d8ed90e1
John Page <john.page@10gen.com>
2022-07-26 15:33:19 +0100

    x

--- hosting/files/appConfig.js
+++ hosting/files/appConfig.js
@@ -1,3 +1,7 @@
+//Change this so your browser app knows what atlas app services to connect to
+
 const atlasAppConfig = { ATLAS_SERVICES_APPID : "atlasforms-xrpys" }
+
+//All the strings that aren't in HTML to allow localization
 
 const appStrings = { AF_INCORRECT_PASSWORD : "Incorrect username or password."}
