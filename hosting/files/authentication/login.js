ddeb5d013549c3e1b3ed61df13598892d73b6452
John Page <john.page@10gen.com>
2022-07-26 15:11:19 +0100

    x

--- hosting/files/authentication/login.js
+++ hosting/files/authentication/login.js
@@ -14,24 +14,22 @@ function passwordLogin()
 }
 
 function loginOnLoad() {
+    const { createApp } = Vue
     const authHelper = new AuthHelper();
+
     if (authHelper.isLoggedIn()) {
       //We should not be here if we are already logged in
       window.location.replace("formsapp/formsapp.html");
     }
-
-
     
-    const { createApp } = Vue
     vueApp  = createApp({
        methods: {
-        passwordLogin,
+        passwordLogin : authHelper.passwordLogin,
        },
        data() {
         return {
             username: "",
-            password: "",
-            authHelper
+            password: ""
         }
        }
     }).mount("#loginapp")
