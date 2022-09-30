7df5e449571c3a14e9a8d5e3f04ae6835708b08a
John Page <john.page@10gen.com>
2022-09-30 17:02:58 +0100

    .

--- hosting/files/formsapp/formsapp.js
+++ hosting/files/formsapp/formsapp.js
@@ -86,6 +86,10 @@ async function selectDocType() {
   }
 }
 
+function getFieldDataType(name) {
+  console.log(name,selectedDocTypeSchema[name])
+  return vueApp.selectedDocTypeSchema[name];
+}
 
 async function formsOnLoad() {
   const { createApp } = Vue
@@ -99,7 +103,7 @@ async function formsOnLoad() {
   vueApp = createApp({
     methods: {
       //Method we call from  HTML
-      logOut, selectDocType, formValueChange, runQuery, clearForm, editRecord, newRecord, toDateTime, getBsonType, watchColumnResizing, getFieldValue, formatFieldname, sortListviewColumn
+      logOut, selectDocType,getFieldDataType, formValueChange, runQuery, clearForm, editRecord, newRecord, toDateTime, getBsonType, watchColumnResizing, getFieldValue, formatFieldname, sortListviewColumn
 
     },
     data() {
