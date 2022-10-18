387ca5a428a8d3b60ed166f2435d4433acefb3d5
Luce Carter <luce@lucyscarter.co.uk>
2022-10-18 11:30:50 +0100

    Call  subfield in schema

--- hosting/files/formsapp/formsapp.js
+++ hosting/files/formsapp/formsapp.js
@@ -182,7 +182,7 @@ function addArrayElement(name) {
   if(vueApp.selectedDocTypeSchema[name] == undefined || vueApp.selectedDocTypeSchema[name] == null)
   {
     const [arrayname, subfield] = name.split('_');
-    const schemaElement = vueApp.selectedDocTypeSchema[arrayname];
+    const schemaElement = vueApp.selectedDocTypeSchema[arrayname][subfield];
     console.log(schemaElement);
   }
   const elementBsonType = getBsonType(vueApp.selectedDocTypeSchema[name][0])
