0f5666685eecaf162205859e6a1c2edcf8e14981
Luce Carter <luce@lucyscarter.co.uk>
2022-10-26 10:42:59 +0100

    WIP - Initial commit of files to begin import. Adding new function

---
+++ functions/importDocType.js
@@ -0,0 +1,18 @@
+// This is for importing a document passed
+exports = async function importDocType(namespace, name, url, listviewfields) {
+
+    // Get the 
+
+    /*Get an Authorization object - should be standard in any non private function*/
+    const authorization = await context.functions.execute("newAuthorization", context.user.id);
+    if (authorization == null) { return { ok: false, message: "User not Authorized" }; }
+
+    let rval = { ok: false, message: "No Error Message Set" };
+    let postCommit = {};
+
+    const doctypesdata = context.services.get("mongodb-atlas").db("__atlasforms").collection("doctypes");
+    const listViewFieldsArray = listviewfields.split(',');
+
+    const newdoctype = {};
+    
+}
---
+++ hosting/files/import/import.js
@@ -0,0 +1,36 @@
+function importOnLoad() {
+    const { createApp } = Vue
+    
+    const realmApp = new Realm.App({ id: atlasAppConfig.ATLAS_SERVICES_APPID });
+
+    if (realmApp.currentUser == null) {
+      // We should not be here if we are already logged in
+      window.location.replace("/login/login.html");
+    }
+
+    vueApp = createApp({
+        methods: {
+          //Method we call from  HTML
+          //log: console.log,
+          //logOut, selectDocType, formValueChange, runQuery, clearForm     
+        },
+        data() {
+          return {
+            listViewFields: [],          
+          }
+        },
+        mounted() {
+          //Non reactive data , don't want reactivity on a deep component like this.
+          //So we don't add it in the data member
+          //Also confiuses first login attempt for a new user with realmApp.
+          this.realmApp = realmApp;         
+        },
+      }).mount("#importapp");    
+
+      console.log(vueApp.listViewFields);
+}
+
+//User has added listview field names
+async function addListviewFieldName(name) {
+
+}
