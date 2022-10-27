24a8620537426b0c15a520802e34b78fd750919f
Luce Carter <luce@lucyscarter.co.uk>
2022-10-27 12:35:53 +0100

    Fixes to import

--- functions/importDocType.js
+++ functions/importDocType.js
@@ -1,84 +1,85 @@
 // This is for importing a document passed via url
 exports = async function (namespace, importdoctypename, importurl, listviewfields) {
   
+    let rval = { ok: false, message: "No Error Message Set" };
+     
+     /*Get an Authorization object - should be standard in any non private function*/
+     // const authorization = await context.functions.execute("newAuthorization", context.user.id);
+     // if (authorization == null) { return { ok: false, message: "User not Authorized" }; }
+ 
+     const filetoimport = await fetchFile(importurl);
+     
+     if(importurl.slice(-4).toLowerCase() != 'json' )
+     {
+       rval = { ok: false, message: "URL not a json file" };
+     }
+    
+     try {
+         const [db, collection] = namespace.split('.');
+ 
+         const importdb = context.services.get("mongodb-atlas").db(db);
+         const importcoll = importdb.collection(collection);
+         
+         /*
+         App Services doesn't support drop on an existing collection if you want to replace everything, not add to it.
+         This aggregation pipeline is a workaround for this that allows you to essentially generate nothing and use $out to add nothing to the collection.
+         $out is a stage that can overwrite the contents. But it has to be done on a collection and can't take passed in data.
+         If the collection doesn't exist, the pipeline simply won't do anything. So this is a way of emptying a collection if it exists, otherwise doing nothing
+         so it is ready to then have our fetched data written to it.
+         */
+         const emptyCollPipeline = [{$indexStats: {}}, {$match: {luce:"awesome"}}, {$out: collection}];
+         await importcoll.aggregate(emptyCollPipeline).toArray();
+         importcoll.insertMany(filetoimport);
+         
+         await addToDocTypes(namespace,importdoctypename,listviewfields);
+         
+         rval = { ok: true, message: "Succesfully imported to collection" };
+     }
+     catch (e) {
+         console.log(`Error inserting imported file: ${e}`);
+ 
+         return { ok: false, message: `Error inserting imported file: ${e}` }
+     }
+     return rval;
+ }
+ 
+ async function fetchFile(fileUrl) {
+     try {
+         const response = await context.http.get({
+             url: fileUrl
+         });
+        
+         return JSON.parse(response.body.text());
+ 
+     } catch (e) {
+         console.log(`Error: ${e}`);
+     }
+ }
+ 
+ // As well as import the data, we want to record that this is a new entity/doc type
+ async function addToDocTypes(namespace,title,listviewfields) {
    let rval = { ok: false, message: "No Error Message Set" };
-    
-    /*Get an Authorization object - should be standard in any non private function*/
-    // const authorization = await context.functions.execute("newAuthorization", context.user.id);
-    // if (authorization == null) { return { ok: false, message: "User not Authorized" }; }
-
-    const filetoimport = await fetchFile(importurl);
-    
-    if(importurl.slice(-4).toLowerCase() != 'json' )
-    {
-      rval = { ok: false, message: "URL not a json file" };
-    }
    
-    try {
-        const [db, collection] = namespace.split('.');
-
-        const importdb = context.services.get("mongodb-atlas").db(db);
-        const importcoll = importdb.collection(collection);
-        
-        /*
-        App Services doesn't support drop on an existing collection if you want to replace everything, not add to it.
-        This aggregation pipeline is a workaround for this that allows you to essentially generate nothing and use $out to add nothing to the collection.
-        $out is a stage that can overwrite the contents. But it has to be done on a collection and can't take passed in data.
-        If the collection doesn't exist, the pipeline simply won't do anything. So this is a way of emptying a collection if it exists, otherwise doing nothing
-        so it is ready to then have our fetched data written to it.
-        */
-        const emptyCollPipeline = [{$indexStats: {}}, {$match: {luce:"awesome"}}, {$out: collection}];
-        await importcoll.aggregate(emptyCollPipeline).toArray();
-        importcoll.insertMany(filetoimport);
-        
-        await addToDocTypes(namespace,importdoctypename,listviewfields);
-        
-        rval = { ok: true, message: "Succesfully imported to collection" };
-    }
-    catch (e) {
-        console.log(`Error inserting imported file: ${e}`);
-
-        return { ok: false, message: `Error inserting imported file: ${e}` }
-    }
-}
-
-async function fetchFile(fileUrl) {
-    try {
-        const response = await context.http.get({
-            url: fileUrl
-        });
-       
-        return JSON.parse(response.body.text());
-
-    } catch (e) {
-        console.log(`Error: ${e}`);
-    }
-}
-
-// As well as import the data, we want to record that this is a new entity/doc type
-async function addToDocTypes(namespace,title,listviewfields) {
-  let rval = { ok: false, message: "No Error Message Set" };
-  
-  const doctypescoll = context.services.get("mongodb-atlas").db("__atlasforms").collection("doctypes");
-  
-  try {
-    const fields = listviewfields.split(',');
-    
-    if(fields.length >0) {
-      const newDocType = {
-        title: title,
-        namespace: namespace,
-        listViewFields: fields
-      };
-    
-      doctypescoll.replaceOne({title: title}, newDocType, {upsert: true});
-      rval = {ok: true, message: `Successfully added new doc type`};
-    }
-    
-    rval = {ok: false, message: "Listview fields not present" };
-  } 
-  catch (e) {
-    console.log(`Error adding to DocTypes: ${e}`)
-    rval = {ok: false, message: `Error adding to DocTypes: ${e}`};
-  }
-}
+   const doctypescoll = context.services.get("mongodb-atlas").db("__atlasforms").collection("doctypes");
+   
+   try {
+     const fields = listviewfields.split(',');
+     
+     if(fields.length >0) {
+       const newDocType = {
+         title: title,
+         namespace: namespace,
+         listViewFields: fields
+       };
+     
+       doctypescoll.replaceOne({title: title}, newDocType, {upsert: true});
+       rval = {ok: true, message: `Successfully added new doc type`};
+     }
+     
+     rval = {ok: false, message: "Listview fields not present" };
+   } 
+   catch (e) {
+     console.log(`Error adding to DocTypes: ${e}`)
+     rval = {ok: false, message: `Error adding to DocTypes: ${e}`};
+   }
+ }
--- hosting/files/import/import.js
+++ hosting/files/import/import.js
@@ -10,13 +10,15 @@ function importOnLoad() {
 
     vueApp = createApp({
         methods: {
-          //Method we call from  HTML
-          //log: console.log,
-          //logOut, selectDocType, formValueChange, runQuery, clearForm     
+          importDocType  
         },
         data() {
           return {
-            listViewFields: [],          
+            namespace: "",
+            importdoctypename: "",
+            importurl: "",
+            listviewfields: "",
+            message: ""          
           }
         },
         mounted() {
@@ -25,13 +27,12 @@ function importOnLoad() {
           //Also confiuses first login attempt for a new user with realmApp.
           this.realmApp = realmApp;         
         },
-      }).mount("#importapp");    
-
-      console.log(vueApp.listViewFields);
+      }).mount("#importapp");          
 }
-async function importDocType(namespace, name, url, listviewfields) {
+
+async function importDocType(namespace,importdoctypename,importurl,listviewfields) {
     try {
-      let { ok, message } = await vueApp.realmApp.currentUser.functions.importDocType(namespace,name,listviewfields,url)
+      let { ok, message } = await vueApp.realmApp.currentUser.functions.importDocType(namespace,importdoctypename,importurl,listviewfields)
     }
     catch(e) {
 
