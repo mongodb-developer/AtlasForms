cdfec0a265174054348548471ed9764838acd987
John Page <johnlpage@gmail.com>
2022-11-22 13:23:19 +0000

    Made Links work on Nested fields, removed links from arrays for now.

--- hosting/files/appConfig.js
+++ hosting/files/appConfig.js
@@ -1,6 +1,6 @@
 //Change this so your browser app knows what atlas app services to connect to
 
-const atlasAppConfig = { ATLAS_SERVICES_APPID : "atlasforms-xxxab" }
+const atlasAppConfig = { ATLAS_SERVICES_APPID : "atlasforms-fiewc" }
 
 //All the strings that aren't in HTML to allow localization
 //Avoid showing end users errors returned from the server, log them instead.
